e6968af0d8136460700874be48d6b7cd5eede379
saurav1812 <shorya181299@gmail.com>
2024-03-13 00:36:48 +0530

    account db created

--- paytm-main/backend/db.js
+++ paytm-main/backend/db.js
@@ -32,8 +32,21 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+const accountSchema = new mongoose.Schema({
+  userId: {
+       type: mongoose.Schema.Types.ObjectId,
+       ref: 'User',
+       required: true
+  },
+  balance: {
+    type: Number,
+    required: true
+  }
+});
+const Account = mongoose.model('Account',accountSchema)
 const user = mongoose.model('user',userSchema);
 
 module.exports = {
-    user
+    user,
+    userSchema
 };
