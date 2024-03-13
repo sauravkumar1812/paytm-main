7e8837dc64bf81c9fa892b2c2f8bb96502c2876a
saurav1812 <shorya181299@gmail.com>
2024-03-13 22:43:51 +0530

    normal commit

--- paytm-main/backend/middleware.js
+++ paytm-main/backend/middleware.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
     return res.status(403).json({});
   }
 
