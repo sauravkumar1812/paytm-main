5ab2ce85fed9bb5c34b83ae6f5ff605e99932c31
saurav1812 <shorya181299@gmail.com>
2024-03-23 20:30:31 +0530

    Backend Completed

--- paytm-main/frontend/src/App.jsx
+++ paytm-main/frontend/src/App.jsx
@@ -5,7 +5,8 @@ function App() {
    <>
    <BrowserRouter>
         <Routes>
-          <Route path="/signup" element={<Signup />} />
+          {/* routing all pages  */}
+          <Route path="/signup" element={<Signup />} />    
           <Route path="/signin" element={<Signin />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/send" element={<SendMoney />} />
